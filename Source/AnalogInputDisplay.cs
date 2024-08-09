306bfa6d06775d7f1a1e44462bf6b454e0b37104
Jessica Chen <jessica.scoltock@protonmail.com>
2024-08-08 20:20:10 -0400

    Clean up settings

--- Source/AnalogInputDisplay.cs
+++ Source/AnalogInputDisplay.cs
@@ -13,12 +13,14 @@ public class AnalogInputDisplay : Entity
 
     public override void Render()
     {
+        AnalogInputDisplayModuleSettings settings = AnalogInputDisplayModule.Settings;
+        if (!settings.Enabled) return;
+
         Player player = Scene.Tracker.GetEntity<Player>();
         Level level = SceneAs<Level>();
         if (level == null) return;
         if (player == null) return;
 
-        AnalogInputDisplayModuleSettings settings = AnalogInputDisplayModule.Settings;
         float radius = settings.Radius * 5f;
         Vector2 pos = level.WorldToScreen(player.Center);
 
@@ -26,8 +28,8 @@ public class AnalogInputDisplay : Entity
         if (gamepad.ThumbSticks.Left.Length() > settings.MinDisplayThreshold)
         {
             Vector2 thumbstick = new Vector2(gamepad.ThumbSticks.Left.X, -gamepad.ThumbSticks.Left.Y);
-            drawIndicator(pos, thumbstick, settings.IndicatorLength, radius, Color.Red, settings.InputThickness);
-            drawIndicator(pos, Input.LastAim, settings.IndicatorLength, radius, Color.White, settings.ActualDirectionThickness);
+            drawIndicator(pos, thumbstick, radius, settings.InputIndicator);
+            drawIndicator(pos, Input.LastAim, radius, settings.DashIndicator);
         }
     }
 
@@ -37,10 +39,10 @@ public class AnalogInputDisplay : Entity
         return initial + offset;
     }
 
-    private void drawIndicator(Vector2 origin, Vector2 direction, float length, float radius, Color color, float thickness = 1)
+    private void drawIndicator(Vector2 origin, Vector2 direction, float radius, AnalogInputDisplayModuleSettings.Indicator indicator)
     {
-        Vector2 start = translateVec(origin, direction.Angle(), radius - length);
+        Vector2 start = translateVec(origin, direction.Angle(), radius - indicator.Length);
         Vector2 end = translateVec(origin, direction.Angle(), radius);
-        Draw.Line(start, end, color, thickness);
+        Draw.Line(start, end, indicator.GetXNAColor(), indicator.Thickness);
     }
 }
--- Source/AnalogInputDisplayModuleSettings.cs
+++ Source/AnalogInputDisplayModuleSettings.cs
@@ -1,3 +1,6 @@
+using System;
+using Microsoft.Xna.Framework;
+
 namespace Celeste.Mod.AnalogInputDisplay;
 
 public class AnalogInputDisplayModuleSettings : EverestModuleSettings
@@ -6,15 +9,45 @@ public class AnalogInputDisplayModuleSettings : EverestModuleSettings
 
     [SettingRange(1, 100, true)]
     public int Radius { get; set; } = 20;
-    
-    [SettingRange(1, 20)]
-    public int IndicatorLength { get; set; } = 10;
 
     public float MinDisplayThreshold { get; set; } = 0.1f;
 
-    [SettingRange(1, 10)]
-    public int InputThickness { get; set; } = 2;
+    public Indicator InputIndicator { get; set; } = new Indicator(ColorSetting.White);
 
-    [SettingRange(1, 10)]
-    public int ActualDirectionThickness { get; set; } = 3;
+    public Indicator DashIndicator { get; set; } = new Indicator(ColorSetting.Gray);
+
+    [SettingSubMenu]
+    public class Indicator {
+        public Indicator() {}
+        public Indicator(ColorSetting defaultColor) {
+            Color = defaultColor;
+        }
+
+        public Color GetXNAColor() {
+            string colorName = Enum.GetName(typeof(ColorSetting), Color);
+            Color color = (Color)(typeof(Color).GetProperty(colorName)).GetValue(null, null);
+
+            return color;
+        }
+
+        public ColorSetting Color { get; set;} = ColorSetting.White;
+
+        [SettingRange(1, 10)]
+        public int Thickness { get; set; } = 2;
+
+        [SettingRange(1, 20)]
+        public int Length { get; set; } = 10;
+    }
+}
+
+public enum ColorSetting {
+    White,
+    Gray,
+    Black,
+    Red,
+    Green,
+    Blue,
+    Cyan,
+    Magenta,
+    Yellow,
 }
